4698e480b0bdf502a6b006e052a6ba2dd287c256
kim-yu <kimberly.yu1@gmail.com>
2018-10-07 00:06:04 -0700

    feature-3: split words into separate divs

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     const verse = "Keep your heart with all diligence, For out of it spring the issues of life.";
     const reference = "Proverbs 4:23";
 
-    const originalWords = verse.split()
+    const originalWords = verse.split(" ")
     var isVisible = {};
     for (i=0; i<originalWords.length; i++) {
         isVisible[i] = true;
@@ -10,10 +10,18 @@ $(document).ready(() => {
 
     var verseDiv = document.getElementById("verse");
     var visibleWords = "";
+    var i;
     for (i=0; i<originalWords.length; i++) {
+        var wordDiv = document.createElement('div');
+        wordDiv.id = i;
+        wordDiv.innerHTML = originalWords[i]
+        wordDiv.classList.add('word');
         if (isVisible[i]) {
             visibleWords += originalWords[i] + " ";
+            wordDiv.classList.add('visible');
+        } else {
+            wordDiv.classList.add('hidden');
         }
+        verseDiv.appendChild(wordDiv);
     }
-    verseDiv.innerHTML = visibleWords.substring(0, visibleWords.length-1)
 });
