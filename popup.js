85f01f8d0149ae0be69792d34dbbeb3b84578b7f
kim-yu <kimberly.yu1@gmail.com>
2019-01-28 00:05:14 -0800

    feature-2: toggle word visibility in Edit mode

--- popup.js
+++ popup.js
@@ -10,66 +10,18 @@ $(document).ready(() => {
 
     var inEditMode = false;
 
-    var verseDiv = document.getElementById("verse");
-    var visibleWords = "";
+    let verseDiv = document.getElementById("verse");
     var i;
+
     for (i=0; i<originalWords.length; i++) {
-        var contentDiv = document.createElement('div');
-        contentDiv.id = i;
-        contentDiv.classList.add('content');
         var wordDiv = document.createElement('div');
         wordDiv.id = "word-"+i;
         wordDiv.innerHTML = originalWords[i]
         wordDiv.classList.add('word');
-        
-        var buttonSpan = document.createElement('span');
-        buttonSpan.id = "span-"+i;
-        var buttonDiv = document.createElement('i');
-        buttonDiv.classList.add('fas');
-        
-        if (isVisible[i]) {
-            visibleWords += originalWords[i] + " ";
-            wordDiv.classList.add('visible');
-            buttonDiv.classList.add('fa-eye-slash');
-        } else {
-            wordDiv.innerHTML = " " * originalWords[i].length
-            buttonDiv.classList.add('fa-eye');
-        }
+        wordDiv.classList.add('visible');
     
-        buttonSpan.appendChild(buttonDiv)
-        contentDiv.appendChild(wordDiv);
-        contentDiv.appendChild(buttonSpan);
-        verseDiv.appendChild(contentDiv);
+        verseDiv.appendChild(wordDiv);
     }
-
-    $.fn.changeElementType = function(newType) {
-        var attrs = {};
-
-
-        $.each(this[0].attributes, function(idx, attr) {
-            attrs[attr.nodeName] = attr.nodeValue;
-        });
-
-
-        var newelement = $("<" + newType + "/>", attrs).append($(this).contents());
-        this.replaceWith(newelement);
-        return newelement;
-    };
-
-    $('span').click(function (event) {
-        if ($(event.target).parent().siblings('.word').hasClass('visible')) {
-            $(event.target).parent().siblings('.word').removeClass('visible');
-            $(event.target).parent().siblings('.word').addClass('hidden');
-            $(event.target).parent().children('.fas').removeClass('fa-eye-slash');
-            $(event.target).parent().children('.fas').addClass('fa-eye');
-        } else if ($(event.target).parent().siblings('.word').hasClass('hidden')) {
-            $(event.target).parent().siblings('.word').removeClass('hidden');
-            $(event.target).parent().siblings('.word').addClass('visible');
-            $(event.target).parent().children('.fas').removeClass('fa-eye');
-            $(event.target).parent().children('.fas').addClass('fa-eye-slash');
-        }
-        $(event.target).css('color', 'transparent');
-    });
 
     $('button').click(function (event) {
     	if (inEditMode) {
@@ -80,6 +32,26 @@ $(document).ready(() => {
     		inEditMode = true
     		$('button').css('background-color', '#2196F3');
     		$('button').css('color', 'white');
+    	}
+    });
+
+    $('.word').click(function (event) {
+    	let id = $(this).attr('id');
+    	var i = id.substring(5);
+    	var word = document.getElementById(id);
+
+    	if (inEditMode) {
+    		if ($(event.target).hasClass('visible')) {
+	            $(event.target).removeClass('visible');
+	            $(event.target).addClass('hidden');
+	            word.innerHTML = originalWords[i].replace(new RegExp('[gqp]'),'b').replace('j','i').replace('y','v'); // use placeholders for letters with descenders
+	            isVisible[i] = false;
+	        } else if ($(event.target).hasClass('hidden')) {
+	            $(event.target).removeClass('hidden');
+	            $(event.target).addClass('visible');
+	            word.innerHTML = originalWords[i];
+	            isVisible[i] = true;
+	        }
     	}
     });
 });
