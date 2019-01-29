4f209338346803daad464d80f897a4fa708543f7
kim-yu <kimberly.yu1@gmail.com>
2019-01-28 22:28:39 -0800

    convert blank to textfield when not in edit mode

--- popup.js
+++ popup.js
@@ -7,12 +7,31 @@ $(document).ready(() => {
 	const tokens = verse.split(" "); // split verse into words and punctuation
     var isVisible = {}; // keep track of visibility of words
     for (i=0; i<tokens.length; i++) {
-    	if (!tokens[i].slice(-1).match(/\b/)) {
-			isVisible[i] = true;
+    	isVisible[i] = true;
+    }
+
+    // change visibility of word in edit mode
+    var changeVisibility = function () {
+    	if (inEditMode) {
+    		var id = $(this).attr('id');
+    		var i = id.substring(5);
+    		var wordSpan = document.getElementById(id);
+    		var word = cleanToken(tokens[i]);
+    		if (wordSpan.classList.contains('visible')) {
+    			wordSpan.classList.remove('visible');
+    			wordSpan.classList.add('hidden');
+    			wordSpan.innerHTML = handleDescenders(word);
+    			isVisible[i] = false;
+    		} else if (wordSpan.classList.contains('hidden')) {
+    			wordSpan.classList.remove('hidden');
+    			wordSpan.classList.add('visible');
+    			wordSpan.innerHTML = word;
+    			isVisible[i] = true;
+    		}
     	}
     }
 
-    // create div elements for words
+    // create elements for words
     let verseDiv = document.getElementById("verse");
     var i;
     for (i=0; i<tokens.length; i++) {
@@ -21,6 +40,7 @@ $(document).ready(() => {
         wordSpan.classList.add('token');
         wordSpan.classList.add('word');
         wordSpan.classList.add('visible');
+        $(wordSpan).on('click', changeVisibility);
 
        	let token = tokens[i]
         let word = cleanToken(token) // use the part of the word before punctuation
@@ -41,33 +61,72 @@ $(document).ready(() => {
     		inEditMode = false
     		$('button').css('background-color', 'white');
     		$('button').css('color', 'black');
+    		reloadVerse();
     	} else {
     		inEditMode = true
     		$('button').css('background-color', '#2196F3');
     		$('button').css('color', 'white');
+    		reloadVerse();
     	}
     });
 
-    // change visibility of word in edit mode
-    $('.word').click(function (event) {
-    	let id = $(this).attr('id');
-    	var i = id.substring(5);
+    // change span to input
+    var switchToInput = function (id) {
     	var wordSpan = document.getElementById(id);
-		var word = cleanToken(tokens[i]);
-    	if (inEditMode) {
-    		if ($(event.target).hasClass('visible')) {
-	            $(event.target).removeClass('visible');
-	            $(event.target).addClass('hidden');
-	            wordSpan.innerHTML = handleDescenders(word);
-	            isVisible[i] = false;
-	        } else if ($(event.target).hasClass('hidden')) {
-	            $(event.target).removeClass('hidden');
-	            $(event.target).addClass('visible');
-	            wordSpan.innerHTML = word;
-	            isVisible[i] = true;
-	        }
-    	}
-    });
+		var width = wordSpan.offsetWidth;
+    	var $input = $("<input>", {
+	        type: "text"
+	    });
+	    $input.attr('id', id);
+	    $input.addClass("blank");
+	    $input.addClass("token");
+	    $input.addClass("word");
+	    $input.width(width);
+	    $(wordSpan).replaceWith($input);
+    }
+
+    // change input to span
+    var switchToSpan = function (id) {
+    	var wordInput = document.getElementById(id);
+    	var word = cleanToken(tokens[i]);
+    	var $span = $("<span>", {
+			text: handleDescenders(word)
+		});
+		$span.attr('id', id);
+		$span.addClass('token');
+		$span.addClass('word');
+		$span.addClass('hidden');
+		$(wordInput).replaceWith($span);
+		$span.on('click', changeVisibility);
+    }
+
+    // reloads elements based on visibility
+	reloadVerse = function () {
+		if (!inEditMode) {
+			for (i=0; i<tokens.length; i++) {
+				var id = "word-"+i;
+				if (!isVisible[i]) {
+					switchToInput(id);
+				}
+			}
+
+			// focus the first textfield in the verse
+		    for (i=0; i<tokens.length; i++) {
+		    	var id = "word-" + i;
+		    	var element = document.getElementById(id);
+		    	if (element.nodeName == "INPUT") {
+		    		$(element).focus();
+		    	}
+		    }
+		} else {
+		    for (i=0; i<tokens.length; i++) {
+		    	var id = "word-"+i;
+		    	if (!isVisible[i]) {
+		    		switchToSpan(id);
+		    	}
+		    }
+		}
+	}
 });
 
 // gets the part of the word before the punctuation
